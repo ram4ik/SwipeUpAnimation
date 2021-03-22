73a16fd6f8dee7fcab0e07b9effee49bf4ffd561
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-03-22 16:17:52 +0200

    Initial Commit

---
+++ SwipeUpAnimation/SwipeUpAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeUpAnimationApp.swift
+//  SwipeUpAnimation
+//
+//  Created by Ramill Ibragimov on 22.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwipeUpAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
